4d1d224d49bfab13f6569ce0927b155f7bc4718c
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-03-10 04:31:10 -0400

    Remove old stuff

--- frontend/pages/home/Home.tsx
+++ frontend/pages/home/Home.tsx
@@ -1,55 +1,21 @@
 import { Global, InterpolationWithTheme } from "@emotion/core";
 import * as React from "react";
-import EventListener from "react-event-listener";
-import { color } from "../../settings/color";
 import { globalCss } from "../../settings/commonCss";
-import { font } from "../../settings/font";
-import { mediaQueries } from "../../settings/mediaQueries";
-import { Content } from "./components/Content";
-import { Head } from "./components/Head";
 
-export class Home extends React.Component<{}, { scrollAmount: number }> {
+export class Home extends React.Component<{}, {}> {
 
     public constructor(props: {}) {
         super(props);
-        this.state = { scrollAmount: 0 };
-    }
-
-    private handleScroll = () => {
-        if (window) {
-            this.setState({ scrollAmount: window.scrollY / window.innerHeight });
-        }
     }
 
     public render() {
         return (
             <React.Fragment>
                 <Global
-                    styles={{
-                        ...this.globalCss as object,
-                        ...this.bodyCss() as object,
-                    }}
+                    styles={globalCss}
                 />
-                <EventListener
-                    target="window"
-                    onScroll={this.handleScroll}
-                />
-                <Head />
-                <Content />
+                <h1>Hello, world!</h1>
             </React.Fragment>
         );
-    }
-
-    private readonly globalCss: InterpolationWithTheme<any> = globalCss;
-
-    private bodyCss(): InterpolationWithTheme<any> {
-        return {
-            body: {
-                ...font.serif,
-                backgroundColor: this.state.scrollAmount <= 0
-                    ? color.darkBackground
-                    : color.lightBackground,
-            },
-        };
     }
 }
--- frontend/settings/commonCss.ts
+++ frontend/settings/commonCss.ts
@@ -5,17 +5,4 @@ export const globalCss = {
         padding: 0,
         margin: 0,
     },
-
-    a: {
-        color: color.link,
-        textDecoration: "none",
-        cursor: "pointer",
-        ":hover": {
-            textDecoration: "underline",
-        },
-        ":visited": {
-            textDecoration: "underline",
-            color: color.visitedLink,
-        },
-    },
 };
